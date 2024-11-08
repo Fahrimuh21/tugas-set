3ca768f066e8a66fa7f5ceb64f44e60d3758143f
Muhammad Fahri <fahrimuh775@gmail.com>
2024-11-08 08:02:52 -0800

    Update set1.py
    nambahin predikat

--- set1.py
+++ set1.py
@@ -13,13 +13,55 @@
 #dan belum pernah mengerjakan kuis (nilainya berupa list kosong). MakeMhs('123', 'Caca', 'C', [90,80,100]) membentuk mahasiswa dengan nim '123', nama 'CC' dari kelas C, 
 #dan telah mengerjakan kuis sebanyak tiga kali dengan nilai masing-masing adalah 90, 80, dan 100. }
 
-#DEFINISI, SPESIFIKASI DAN REALISASI OPERASI LIST YANG DIPERLUKAN UNTUK HIMPUNAN
+#DEFINISI DAN SPESIFIKASI KONSTRUKTOR
+#Konso: elemen, Mhs-> element Mhs
+#Konso(e,L) menghasilkan sebuah Mhs dari e dan L dengan e sebagai elemen pertama
+#Realisasi
+def Konso(e, L):
+    return [e]+L
+
+#Konsi: Mhs -> elemen Mhs
+#Konsi (L, e)-> menghasilkan sebah Mhs dari L dan e dengan e sebagai elemen terakhir
+#Realisasi
+def Konsi(L, e):
+    return L+[e]
+
+
+#DEFINISI DAN SPESIFIKASI SELEKTOR
 #Head: Mhs tidak kosong -> Mhs
 #Head(L): Menghasilkan Mhs tanpa elemen terakhir Mhs, mungkin kosong
 #Realisasi
 def Head(L):
     return L[:-1]
 
+#Tail: Mhs tidak kosong -> List
+#Tail(L): Menghasilkan Mhs tanpa elemen pertama Mhs, mungkin kosong
+#Realisasi
+def Tail(L):
+    return L[1:]
+
+#FirstElmt: Mhs tidak kosong -> elemen
+#FirstElmt(L) Menghasilkan elemen pertama Mhs L
+#Realisasi
+def FirstElmt(L):
+    return L[0]
+
+#LastElmt: Mhs tidak kosong -> elemen
+#LastElmt(L): mengembalikan elemen terakhir terakhir Mhs L
+#Realisasi
+def LastElmt(L):
+    return L[-1]
+
+#NbElmt: Mhs -> integer
+#NbElmt(L): Menghasilkan banyaknya elemen Mhs, nol jika kosong
+#Realisasi
+def NbElmt(L):
+    if IsEmpty(L):
+        return 0
+    else :
+        return 1+NbElmt(Tail(L))
+
+#DEFINISI DAN SPESIFIKASI PREDIKAT
 #IsEmpty : Mhs -> boolean
 #IsEmpty(L) benar jika Mhs kosong
 #Realisasi
@@ -34,9 +76,3 @@ def IsOneElmt(L):
         return False
     else:
         return Tail(L) == [] and Head(L) == []
-    
-#Head: Mhs tidak kosong -> Mhs
-#Head(L): Menghasilkan Mhs tanpa elemen terakhir Mhs, mungkin kosong
-#Realisasi
-def Head(L):
-    return L[:-1]
